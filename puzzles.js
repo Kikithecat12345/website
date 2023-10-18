a757c515a6417a6733063650cbfb7633d19df957
MrMelon54 <github@mrmelon54.com>
2023-10-19 00:27:07 +0100

    OMG total price, melon disfunction v2 electric boobglue

--- puzzles.js
+++ puzzles.js
@@ -1,21 +1,50 @@
+const totalPriceEl = document.getElementById("game-price-total");
+const gp = document.getElementsByClassName("game-price");
+
+var priceData = [];
+
 function loadPrices() {
-  var gp = document.getElementsByClassName("game-price");
-  var gpid = [];
   for (var i = 0; i < gp.length; i++) {
-    gpid.push(gp[i].getAttribute("data-game-id"));
-    gp[i].setAttribute("data-original-price", gp[i].textContent);
-    gp[i].setAttribute("title", "Original price: " + gp[i].textContent);
+    var tick = {
+      el: gp[i],
+      gameId: gp[i].getAttribute("data-game-id"),
+      price: parseFormattedPrice(gp[i].textContent),
+    };
+    priceData.push(tick);
+    tick.el.setAttribute("data-original-price", tick.price);
+    tick.el.setAttribute("title", "Original price: " + tick.price);
+    tick.check = document.createElement("input");
+    tick.check.setAttribute("type", "checkbox");
+    tick.check.setAttribute("checked", "checked");
+    tick.check.addEventListener("change", function () {
+      updateTotalPrice();
+    });
+    tick.el.parentNode.parentNode.insertBefore(tick.check, tick.el.parentNode.nextSibling);
   }
+
+  updateTotalPrice();
+
+  var gpid = priceData
+    .map(function (x) {
+      return x.gameId;
+    })
+    .filter(function (x, i, a) {
+      return a.indexOf(x) === i;
+    });
+
   var x = new XMLHttpRequest();
   x.open("GET", "https://api.kikicat123.ca/v1/sapi/appdetails?filters=price_overview&cc=CA&appids=" + gpid.join(","));
   x.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var j = JSON.parse(this.responseText);
-      for (var i = 0; i < gp.length; i++) {
-        var k = j[gp[i].getAttribute("data-game-id")];
+      for (var i = 0; i < priceData.length; i++) {
+        var tick = priceData[i];
+        var k = j[tick.gameId];
         if (k.success && k.data.price_overview.currency == "CAD") {
-          gp[i].setAttribute("data-current-price", k.data.price_overview.final);
-          gp[i].textContent = formattedPrice(k.data.price_overview.final);
+          tick.price = k.data.price_overview.final;
+          tick.el.setAttribute("data-current-price", tick.price);
+          tick.el.textContent = formattedPrice(tick.price);
+          updateTotalPrice();
         }
       }
     }
@@ -23,9 +52,29 @@ function loadPrices() {
   x.send();
 }
 
+function updateTotalPrice() {
+  totalPriceEl.textContent = formattedPrice(
+    priceData
+      .filter(function (x) {
+        return x.check.checked;
+      })
+      .map(function (x) {
+        return x.price;
+      })
+      .reduce(function (a, b) {
+        return a + b;
+      })
+  );
+}
+
 function formattedPrice(x) {
   x = x.toString();
   return "C$" + x.slice(0, -2) + "." + x.slice(-2);
+}
+
+function parseFormattedPrice(x) {
+  if (x.slice(0, 2) !== "C$") console.error("Default HTML page has invalid price formatting");
+  return parseInt(x.slice(2).split(".").join(""));
 }
 
 window.addEventListener("load", function () {
